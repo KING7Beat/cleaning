cec3cfd29759b3de33f45376f7349b9e9619da6a
Oleg Shvets <oleg.shvets@fluidweb.io>
2020-09-22 19:51:30 +0300

    day 3

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,15 @@ $('.section-questions-accord__item').each(function () {
     {
         $(this).children().eq(1).slideDown();
     }
-})
+});
 
 $('.section-questions-accord__item').click(function () {
+    if ($(this).hasClass('active__acord'))
+    {
+        $(this).children().eq(1).slideUp();
+        $(this).removeClass('active__acord');
+        return false;
+    }
     for (let i=0; i<document.querySelectorAll('.section-questions-accord__item').length; i++)
     {
         document.querySelectorAll('.section-questions-accord__item')[i].classList.remove('active__acord');
@@ -13,4 +19,31 @@ $('.section-questions-accord__item').click(function () {
     }
     $(this)[0].classList.add('active__acord');
     $(this).children().eq(1).slideDown();
+});
+
+$('.form-block_amount-btn').click(function() {
+    let num = parseInt($('.form-block_amount-text').text());
+
+    if ($(this).data('type') === 'minus')
+    {
+        if (num === 0)
+        {
+            return false;
+        }
+
+        $('.form-block_amount-text').text(num-=1);
+    }
+
+    if ($(this).data('type') === 'plus') 
+    {
+        $('.form-block_amount-text').text(num+=1);
+    }
+})
+
+
+$('#dropdown-phone').on('shown.bs.dropdown', function () {
+    $('#dropdown-phone .dropdown-item').click(function () {
+        $('#dropdown-phone .dropdown-toggle').text($(this).text());
+        $('.section-header-phone a').attr('href', `+38${$(this).text().match(/\d+/g).join('')}`);
+    })
 })
